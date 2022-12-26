6c5e505fb6cb7c7342474647ab7ca44eb708fe23
Shyla <shailajasrinivas91@gmail.com>
2022-12-25 22:25:20 -0500

    data component updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi'
+import { FaQuoteRight } from 'react-icons/fa'
+import data from './data'
 
 function App() {
   return (
